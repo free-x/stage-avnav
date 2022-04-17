3480eb3976b7543a9408fcc1558efce411d2bf7c
andreas <andreas@wellenvogel.de>
2022-04-17 10:39:15 +0200

    re-add demosource

--- 04-customizing/01-run-chroot.sh
+++ 04-customizing/01-run-chroot.sh
@@ -11,5 +11,14 @@ if [ -f /etc/default/n2kd ] ; then
 	sed -iorig 's/^ *# *CAN_INTERFACE *=.*/CAN_INTERFACE=can0/' /etc/default/n2kd
 	sed -iorig '/##.*CANBOAT/d' $dd/avnav_server.xml
 fi
+DEMOCHART="osm-online.xml"
+demosrc=/usr/lib/avnav/viewer/demo/$DEMOCHART
+if [ -f $demosrc ] ; then
+  dst=$dd/charts
+  if [ -d $dst ] ; then
+    mkdir -p $dst
+  fi
+  cp $demosrc $dst
+fi
 chown -R pi:pi /home/pi/avnav
 
