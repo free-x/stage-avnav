b6e3b8ffebe459f0768412c0ccdd354ad153cd20
free-x <oroitburd@gmail.com>
2024-12-06 15:55:58 +0100

    cleanup & fixes

--- 04-customizing/01-run.sh
+++ 04-customizing/01-run.sh
@@ -7,7 +7,4 @@ install -v -m 775 -o 1000 -g 1000 files/uart_control "${ROOTFS_DIR}/home/${FIRST
 on_chroot << EOF
 echo "set mouse-=a" >> /home/pi/.vimrc
 echo "set mouse-=a" >> /root/.vimrc
-echo "#dtparam=spi=on" >> /boot/config.txt
-echo "#dtoverlay=mcp2515-can0,oscillator=8000000,interrupt=25,spimaxfrequency=1000000" >> /boot/config.txt
-sed -i s/CONF_SWAPSIZE=100/CONF_SWAPSIZE=500/g /etc/dphys-swapfile
 EOF
