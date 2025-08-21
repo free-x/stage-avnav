13bc240a642e31a52c91fef81687a01d8b1713fd
free-x <oroitburd@gmail.com>
2025-08-21 10:14:18 +0200

    partialy revert 0f7b24b

--- 00-add-sources/00-run.sh
+++ 00-add-sources/00-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+install -m 644 files/oss.boating.gpg "${ROOTFS_DIR}/usr/share/keyrings"
 install -m 644 files/extra.sources "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/extra.sources"
 sed -i "s/ARCH/$(dpkg --print-architecture)/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/extra.sources"
