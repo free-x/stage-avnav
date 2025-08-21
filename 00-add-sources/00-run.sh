0f7b24bcfd0b611de140e3e1895117014112a65d
free-x <oroitburd@gmail.com>
2025-08-21 10:09:19 +0200

    adjust for trixie, cleanup old stuff

--- 00-add-sources/00-run.sh
+++ 00-add-sources/00-run.sh
@@ -1,19 +1,11 @@
 #!/bin/bash -e
 
-if [ ${RELEASE} == "buster" ]
-then
-  install -m 644 files/extra.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
-  on_chroot apt-key add - < files/oss.boating.gpg.key
-  #on_chroot apt-key add - < files/open-mind.space.gpg.key
-else
-  install -m 644 files/extra-bullseye.list "${ROOTFS_DIR}/etc/apt/sources.list.d/extra.list"
-  install -m 644 files/oss.boating.gpg "${ROOTFS_DIR}/usr/share/keyrings"
-fi
-
-sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/extra.list"
+install -m 644 files/extra.sources "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/extra.sources"
+sed -i "s/ARCH/$(dpkg --print-architecture)/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/extra.sources"
 
 if [ "$AVNAV_DAILY" == "1" ] ; then
-  sed -i "s/#deb/deb/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/extra.list"
+    sed -i "s/^#//g" "${ROOTFS_DIR}/etc/apt/sources.list.d/extra.sources"
 fi
   
 
