a66a7b86a05aeebfbbb5fc392ac5dacd5976b4ce
free-x <oroitburd@gmail.com>
2025-08-22 14:39:39 +0200

    again arch try

--- 00-add-sources/00-run.sh
+++ 00-add-sources/00-run.sh
@@ -3,6 +3,7 @@
 install -m 644 files/oss.boating.gpg "${ROOTFS_DIR}/usr/share/keyrings"
 install -m 644 files/extra.sources "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/extra.sources"
+sed -i "s/ARCH/${ARCH}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/extra.sources"
 
 if [ "$AVNAV_DAILY" == "1" ] ; then
     sed -i "s/^#//g" "${ROOTFS_DIR}/etc/apt/sources.list.d/extra.sources"
